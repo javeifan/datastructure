91d804b1307d4d509c5353b1ef353c54f1b1eb88
Gordon Fan <Gordon.Fan@Truecommerce.com>
2021-10-27 17:46:47 +0800

    first commit

---
+++ src/DataUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureWindowFormApp.src
+{
+    class DataUtil
+    {
+        public static int[] getRandomIntArray(int arrayLength,int lowerBound,int upperBound)
+        {
+
+            return null;
+        }
+    }
+}
